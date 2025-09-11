28245ece5049a0a7ca77a57b83d019edd99f596f
karekl <karekl-25@sunderbyfolkhogskola.se>
2025-09-11 22:55:11 +0200

    Initial commit, added unity files

---
+++ Assets/MyScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    string text = "Hello world!";
+    [SerializeField] string myNme = "Tom";
+    public string myHomeTown = "Piteå";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print(text);
+        print("Hello world");
+        Debug.Log(text); //Unity Debug Only
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
